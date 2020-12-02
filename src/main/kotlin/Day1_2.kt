76cbe23336872c99e2c5d990d32ec0b10464be40
Norbert Kiesel <nk@iname.com>
2020-12-02 14:53:29 -0800

    updated gitignore, minor code formatting

--- src/main/kotlin/Day1_2.kt
+++ src/main/kotlin/Day1_2.kt
@@ -5,6 +5,7 @@ import kotlin.io.path.readLines
 @ExperimentalPathApi
 fun main() {
     val input = Path("input1").readLines().map { it.toInt() }
+
     input.forEachIndexed { index1, line1 ->
         input.drop(index1 + 1).forEachIndexed { index2, line2 ->
             input.drop(index2 + 1).forEachIndexed { _, line3 ->
