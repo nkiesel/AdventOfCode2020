04e2550cc9409129c01d6e211d28dec19b117e1f
Norbert Kiesel <nkiesel@metricstream.com>
2021-07-14 14:54:35 -0700

    Added version inspired by Kotlin blog

--- src/test/kotlin/Day1.kt
+++ src/test/kotlin/Day1.kt
@@ -7,35 +7,45 @@ class Day1 {
     @Test
     fun test() {
         val input = Path("input/1").readLines()
-        assertEquals(970816, one(input))
-        assertEquals(96047280, two(input))
+        assertEquals(970816, one(input, 2020))
+        assertEquals(970816, oneK(input, 2020))
+        assertEquals(96047280, two(input, 2020))
     }
 
-    private fun one(input: List<String>): Int {
+    private fun one(input: List<String>, sum: Int): Int? {
         val expenses = input.map { it.toInt() }
 
         expenses.forEachIndexed { index1, line1 ->
             for (line2 in expenses.drop(index1 + 1)) {
-                if (line1 + line2 == 2020) {
+                if (line1 + line2 == sum) {
                     return line1 * line2
                 }
             }
         }
-        return 0
+        return null
     }
 
-    private fun two(input: List<String>): Int {
+    // Solution inspired by code from https://blog.jetbrains.com/kotlin/2021/07/advent-of-code-in-idiomatic-kotlin/
+    private fun oneK(input: List<String>, sum: Int): Int? {
+        val expenses = input.map(String::toInt)
+        // The filter below ensures that we ignore the first 1010 we encounter
+        var seen = 0
+        val complements = expenses.filterNot { sum == it * 2 && seen++ == 0 }.associateBy { sum - it }
+        return expenses.firstNotNullOfOrNull { number -> complements[number]?.let { number * it } }
+    }
+
+    private fun two(input: List<String>, sum: Int): Int? {
         val expenses = input.map { it.toInt() }
 
         expenses.forEachIndexed { index1, line1 ->
             expenses.drop(index1 + 1).forEachIndexed { index2, line2 ->
                 expenses.drop(index2 + 1).forEachIndexed { _, line3 ->
-                    if (line1 + line2 + line3 == 2020) {
+                    if (line1 + line2 + line3 == sum) {
                         return line1 * line2 * line3
                     }
                 }
             }
         }
-        return 0
+        return null
     }
 }
