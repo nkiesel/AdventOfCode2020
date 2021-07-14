f9d0cbfc0fb157809e3fa9af75c66e76e24ee56a
Norbert Kiesel <nkiesel@metricstream.com>
2021-07-14 15:02:35 -0700

    Added solution from VN for day 5

--- src/test/kotlin/Day5.kt
+++ src/test/kotlin/Day5.kt
@@ -8,6 +8,7 @@ class Day5 {
     fun test() {
         val input = Path("input/5").readLines()
         assertEquals(935, one(input))
+        assertEquals(935, vn_one(input))
         assertEquals(743, two(input))
     }
 
@@ -23,5 +24,29 @@ class Day5 {
 
     private fun two(input: List<String>): Int {
         return input.map { seatId(it) }.sorted().zipWithNext().find { it.second != it.first + 1 }!!.first + 1
+    }
+
+    data class Seat(val row: Int, val column: Int, val id: Int)
+
+    private fun vn_one(input: List<String>): Int {
+        val seats = input.map { line -> computeSeat(line) }
+        return seats.maxOf { it.id }
+    }
+
+    private fun computeSeat(input:String): Seat {
+        var startRow = 0
+        var endRow = 127
+        var startColumn = 0
+        var endColumn = 7
+        for (item in input) {
+            when (item) {
+                'B' -> startRow = ((startRow + endRow) / 2) + 1
+                'F' -> endRow = (startRow + endRow) / 2
+                'L' -> endColumn = (startColumn + endColumn) / 2
+                'R' -> startColumn = ((startColumn + endColumn) / 2) + 1
+            }
+        }
+        val seatId = (endRow * 8) + endColumn
+        return Seat(endRow, endColumn, seatId)
     }
 }
